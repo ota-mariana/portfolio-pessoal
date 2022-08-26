cfef355352fc49f2dd915a7931d9752bc62dd6e7
Mariana <nanomarii10@gmail.com>
2022-08-26 11:33:01 -0300

    Limpando o CSS da App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Portfólio em processo...</h2>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Portfólio em processo...</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
